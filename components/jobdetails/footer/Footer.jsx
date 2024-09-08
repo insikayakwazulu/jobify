e5794878540bffa2d7405bf1e464d38697fbd5e3
insikayakwazulu <sphelelesfisokuhle@gmail.com>
2024-09-08 20:23:52 +0200

    first commit

---
+++ components/jobdetails/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
+
+import styles from "./footer.style";
+import { icons } from "../../../constants";
+
+const Footer = ({ url }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.likeBtn}>
+        <Image
+          source={icons.heartOutline}
+          resizeMode='contain'
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.applyBtn}
+        onPress={() => Linking.canOpenURL(url).then(supported => {
+          if (supported) {
+            Linking.openURL(url);
+          } else {
+            console.log("Don't know how to open URI: " + url);
+          }
+        })}
+      >
+       <Text style={styles.applyBtnText}>Apply for job</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Footer;
